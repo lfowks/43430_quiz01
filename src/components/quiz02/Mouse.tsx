7a12c6ab3f681432405f561037bdf6731e1c2244
nayelisilva06 <nayelizunigasilva@gmail.com>
2024-03-20 18:28:13 -0600

    prueba

--- src/components/quiz02/Mouse.tsx
+++ src/components/quiz02/Mouse.tsx
@@ -1,16 +1,12 @@
 
-import { useEffect } from "react";
-import Keyboard from "./Keyboard";
-
-const Mouse = () =>  {
-  const[quantity, setQuantity]= useState(0);
+const Mouse = (cantidad, setCantidad) =>  {
+  
   const price = 60;
-  const increaseQuantity = () => {
-    setQuantity(quantity + 1);
-  };
-useEffect([]) => {
+ 
 
-}
+
+
+
   return (
     <div className="flex">
      <div className="card">Mouse ${price}</div>
