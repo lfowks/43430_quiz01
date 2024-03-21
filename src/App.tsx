dec2baa543ae2f8c423765a1dbb07e8bab20421b
YOILIN CASTRILLO ALFARO <yoilin.castrillo.alfaro@est.una.ac.cr>
2024-03-20 18:20:12 -0600

    yoilin

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import './App.css'
 import Mouse from './components/quiz02/Mouse'
 import Monitor from './components/quiz02/Monitor'
 import Total from './components/quiz02/Total'
+import { useEffect, useState } from 'react'
+
 
 function App() {
+
+  const [ingresar, setIngresar] = useState();
+
   return (
     <>
       <h1>Quiz 02_504310350</h1>
       <div className='flex'>
-        <Mouse/>
-        <Monitor/>
+        <Mouse ingresar={ingresar}/>
+        <Monitor ingresar={ingresar}/>
       </div>
       <div className='totals'>
         <Total/>
       </div>
+      
     </>
   )
 }
--- src/components/quiz02/Mouse.tsx
+++ src/components/quiz02/Mouse.tsx
@@ -1,14 +1,16 @@
 
 import Keyboard from "./Keyboard";
 
-const Mouse = () => {
+const Mouse = ({ingresar}) => {
 
   const price = 60;
 
   return (
     <div className="flex">
      <div className="card">Mouse ${price}</div>
+    
      <Keyboard/>
+     {ingresar}
     </div>
   )
 }
